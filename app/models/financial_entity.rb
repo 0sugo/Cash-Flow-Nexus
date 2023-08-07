e933d79ca26e470e6eebbeb8cd3bd7bd43950e4a
0sugo <josugo26@gmail.com>
2023-08-07 17:51:27 +0300

    Add validations

--- app/models/financial_entity.rb
+++ app/models/financial_entity.rb
@@ -1,4 +1,7 @@
 class FinancialEntity < ApplicationRecord
   belongs_to :author
   belongs_to :financial_group
+
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { only_integer:true, greater_than_or_equal_to:0 }
 end
--- app/models/financial_group.rb
+++ app/models/financial_group.rb
@@ -1,4 +1,7 @@
 class FinancialGroup < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :financial_entities
+
+  validates :name, presence: true
+  validates :icon, presence: true
 end
