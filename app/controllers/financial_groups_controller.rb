735fb9ae2c0389f848ef1c60b89fb43c8a6b48c9
0sugo <josugo26@gmail.com>
2023-08-10 19:44:38 +0300

    Add controller logic for finanial groups

--- app/controllers/financial_groups_controller.rb
+++ app/controllers/financial_groups_controller.rb
@@ -1,12 +1,35 @@
 class FinancialGroupsController < ApplicationController
-  before_action :authenticate_user!,except: %i[index show]
+  before_action :authenticate_user!
 
   def index
-    @currentUser = if user_signed_in?
-      current_user.name
-    else
-        'Guest'
-      end
+    @currentUser = current_user.name
+    @allgroups = current_user.financial_groups
+    @totalAmounts = {}
+
+    @allgroups.each do |group|
+      @totalAmounts[group] = group.financial_entities.sum(:amount)
+    end
+  end
+  
+  def new
+    @newFinancialGroup = FinancialGroup.new
   end
 
+  def create
+    @newFinancialGroup = FinancialGroup.new(financial_group_params)
+    @newFinancialGroup.user = current_user
+
+    if @newFinancialGroup.save
+      redirect_to financial_groups_path,notice: 'Financial Group created succesfully.'
+    else
+      render:new
+    end
+  end
+
+
+  private
+
+  def financial_group_params
+    params.require(:financial_group).permit(:name, :icon)
+  end
 end
