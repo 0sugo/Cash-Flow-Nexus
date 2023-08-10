2aa49f01d6b8304f4d0a8838765ef9d40f580fe2
0sugo <josugo26@gmail.com>
2023-08-10 19:33:28 +0300

    Add Final schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_07_135943) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_09_154525) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "categorisations", force: :cascade do |t|
+    t.bigint "financial_group_id", null: false
+    t.bigint "financial_entity_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["financial_entity_id"], name: "index_categorisations_on_financial_entity_id"
+    t.index ["financial_group_id"], name: "index_categorisations_on_financial_group_id"
+  end
 
   create_table "financial_entities", force: :cascade do |t|
     t.bigint "author_id", null: false
@@ -26,8 +63,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_07_135943) do
   create_table "financial_groups", force: :cascade do |t|
     t.string "name"
     t.string "icon"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_financial_groups_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,15 +76,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_07_135943) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "categorisations", "financial_entities"
+  add_foreign_key "categorisations", "financial_groups"
   add_foreign_key "financial_entities", "users", column: "author_id"
+  add_foreign_key "financial_groups", "users"
 end
