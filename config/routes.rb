4fa95941f51b4254546d0cd28b565890692d7770
0sugo <josugo26@gmail.com>
2023-08-10 19:36:37 +0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  # get 'home', to: 'home#index', as: 'home'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'home#index'
+  root "home#index"
+
   resources :financial_groups do
   resources :financial_entities
   end
